722e5e2f1bd69f0b3e89d12beaa4b7cb6877982c
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-01-28 21:55:42 +0800

    Modified most of the files

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [message, setMessage] = useState("")
+
+  let onchange = (event) => {
+    const newVal = event.target.value;
+    setMessage(newVal)
+  }
 
   return (
     <div className="App">
-      
+      <div className="bubble">
+        {message}
+      </div>
+      <input type="text" className="textInput" placeholder='Put your text' onChange={onchange} />
     </div>
   )
 }
